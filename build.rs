1781a2b9457b5be08a2c536c48cb6a1b00c847be
Frank Elsinga <frank@elsinga.de>
2025-04-09 00:49:01 +0200

    make sure that downloading files works as intended

--- build.rs
+++ build.rs
@@ -22,6 +22,60 @@ impl CfgBool for cmake::Config {
     }
 }
 
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum RenderingBackend {
+    Metal,
+    OpenGL,
+    Vulkan,
+}
+impl RenderingBackend {
+    fn from_features() -> Self {
+        let with_opengl = env::var("CARGO_FEATURE_OPENGL").is_ok();
+        let with_metal = env::var("CARGO_FEATURE_METAL").is_ok();
+        let with_vulkan = env::var("CARGO_FEATURE_VULKAN").is_ok();
+
+        let is_macos = cfg!(any(target_os = "ios", target_os = "macos"));
+
+        match (with_metal, with_vulkan, with_opengl) {
+            (true, false, false) => Self::Metal,
+            (false, true, false) => Self::Vulkan,
+            (false, false, true) => Self::OpenGL,
+            (false, false, false) => {
+                if is_macos {
+                    Self::Metal
+                } else {
+                    Self::Vulkan
+                }
+            }
+            (_, _, _) => {
+                // TODO: modify for better defaults
+                // This might not be the best logic, but it can change at any moment because it's a fallback with a warning
+                // Current logic: if opengl is enabled, always use that, otherwise pick metal on macOS and vulkan on other platforms
+                println!("cargo::warning=Features 'metal', 'opengl', and 'vulkan' are mutually exclusive.");
+
+                let choice = if with_opengl {
+                    Self::OpenGL
+                } else if is_macos {
+                    Self::Metal
+                } else {
+                    Self::Vulkan
+                };
+                println!("cargo::warning=Using only '{choice}', but this default selection may change in future releases.");
+                choice
+            }
+        }
+    }
+}
+impl std::fmt::Display for RenderingBackend {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RenderingBackend::Metal => f.write_str("metal"),
+            RenderingBackend::OpenGL => f.write_str("opengl"),
+            RenderingBackend::Vulkan => f.write_str("vulkan"),
+        }
+    }
+}
+
 /// Helper that returns a new [`cmake::Config`] with common settings.
 /// It selects the renderer based on Cargo features: the user must enable exactly one of:
 /// "metal", "opengl", or "vulkan". If none are explicitly enabled, on iOS/macOS the default is metal,
@@ -33,42 +87,19 @@ fn create_cmake_config(cpp_root: &Path) -> cmake::Config {
     cfg.define("CMAKE_CXX_COMPILER_LAUNCHER", "ccache");
     cfg.define_bool("MLN_DRAWABLE_RENDERER", true);
 
-    let with_opengl = env::var("CARGO_FEATURE_OPENGL").is_ok();
-    let mut with_metal = env::var("CARGO_FEATURE_METAL").is_ok();
-    let mut with_vulkan = env::var("CARGO_FEATURE_VULKAN").is_ok();
-
-    let is_macos = cfg!(any(target_os = "ios", target_os = "macos"));
-    if !with_opengl && !with_metal && !with_vulkan {
-        if is_macos {
-            with_metal = true;
-        } else {
-            with_vulkan = true;
-        }
-    } else if u8::from(with_metal) + u8::from(with_opengl) + u8::from(with_vulkan) > 1 {
-        // TODO: modify for better defaults
-        // This might not be the best logic, but it can change at any moment because it's a fallback with a warning
-        // Current logic: if opengl is enabled, always use that, otherwise pick metal on macOS and vulkan on other platforms
-        let choice = if with_opengl {
-            with_metal = false;
-            with_vulkan = false;
-            "opengl"
-        } else if is_macos {
-            with_metal = true;
-            with_vulkan = false;
-            "metal"
-        } else {
-            with_vulkan = true;
-            with_metal = false;
-            "vulkan"
-        };
-
-        println!("cargo::warning=Features 'metal', 'opengl', and 'vulkan' are mutually exclusive.");
-        println!("cargo::warning=Using only '{choice}', but this default selection may change in future releases.");
-    }
-
-    cfg.define_bool("MLN_WITH_OPENGL", with_opengl);
-    cfg.define_bool("MLN_WITH_METAL", with_metal);
-    cfg.define_bool("MLN_WITH_VULKAN", with_vulkan);
+    let rendering_backend = RenderingBackend::from_features();
+    cfg.define_bool(
+        "MLN_WITH_OPENGL",
+        rendering_backend == RenderingBackend::OpenGL,
+    );
+    cfg.define_bool(
+        "MLN_WITH_METAL",
+        rendering_backend == RenderingBackend::Metal,
+    );
+    cfg.define_bool(
+        "MLN_WITH_VULKAN",
+        rendering_backend == RenderingBackend::Vulkan,
+    );
     cfg.define_bool("MLN_WITH_WERROR", false);
 
     // The default profile should be release even in a debug mode, otherwise it gets huge
@@ -109,6 +140,74 @@ You may also set MLN_FROM_SOURCE to the path of the maplibre-native directory.
         revision,
         "Unexpected git revision in {dest_disp}, please update the build.rs with the new value '{rev}'",
     );
+}
+
+fn download_static(out_dir: &Path, revision: &str) -> (PathBuf, PathBuf) {
+    let rendering_backend = RenderingBackend::from_features();
+
+    #[cfg(not(any(target_os = "linux", target_os = "macos")))]
+    panic!("unsupported target: only linux and macos are currently supported by maplibre-native");
+    
+    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+    let target = "linux-arm64";
+    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+    let target = "linux-x86";
+    #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+    let target = "macos-arm64";
+
+    let mut tasks = Vec::new();
+    let library_file = out_dir.join(format!(
+        "libmaplibre-native-core-{target}-{rendering_backend}.a"
+    ));
+    if !library_file.is_file() {
+        let static_url=format!("https://github.com/maplibre/maplibre-native/releases/download/core-{revision}/libmaplibre-native-core-{target}-{rendering_backend}.a");
+        println!("cargo:warning=Downloading precompiled maplibre-native core library from {static_url} into {}",out_dir.display());
+        tasks.push(Download::new(&static_url));
+    }
+
+    let headers_file = out_dir.join(format!("maplibre-native-headers.tar.gz"));
+    if !headers_file.is_file() {
+        let headers_url = format!("https://github.com/maplibre/maplibre-native/releases/download/core-{revision}/maplibre-native-headers.tar.gz");
+        println!("cargo:warning=Downloading headers for maplibre-native core library from {headers_url} into {}",out_dir.display());
+        tasks.push(Download::new(&headers_url));
+    }
+    fs::create_dir_all(&out_dir).expect("Failed to create output directory");
+    let mut downloader = Downloader::builder()
+        .download_folder(&out_dir)
+        .parallel_requests(tasks.len() as u16)
+        .build()
+        .expect("Failed to create downloader");
+    let downloads = downloader
+        .download(&tasks)
+        .expect("Failed to download maplibre-native static lib")
+        .into_iter();
+    for download in downloads {
+        if let Err(err) = download {
+            panic!("Unexpected error from downloader: {}", err);
+        }
+    }
+
+    (library_file, headers_file)
+}
+
+/// Extracts the headers from the downloaded tarball
+fn extract_headers(headers_from: &Path, headers_to: &Path) {
+    println!(
+        "cargo:warning=Extracting headers for maplibre-native core library from {} into {}",
+        headers_from.display(),
+        headers_to.display()
+    );
+    let headers_file = fs::File::open(headers_from).expect("Failed to open headers file");
+    let mut tar = flate2::read::GzDecoder::new(headers_file);
+
+    if !headers_to.is_dir() {
+        fs::create_dir_all(headers_to).expect("Failed to create headers directory");
+    }
+    let mut archive = tar::Archive::new(&mut tar);
+    archive.set_overwrite(true);
+    archive
+        .unpack(headers_to)
+        .expect("Failed to extract headers");
 }
 
 fn clone_mln(dir: &Path, repo: &str, revision: &str) {
@@ -192,9 +291,12 @@ fn git<I: IntoIterator<Item = S>, S: AsRef<OsStr>>(dir: &Path, args: I) {
 }
 
 const MLN_GIT_REPO: &str = "https://github.com/maplibre/maplibre-native.git";
-const MLN_REVISION: &str = "3fc93a0b024e34514dafcbb424db93593ff540be";
+const MLN_REVISION: &str = "2544cce75374add864cfd87f13df7a263186f981";
 
-fn clone_or_download(root: &Path) -> PathBuf {
+/// Clone or download maplibre-native into the OUT_DIR
+///
+/// Returns the path to the maplibre-native directory and an optional path to an include directorys.
+fn clone_or_download(root: &Path) -> (PathBuf, Vec<PathBuf>) {
     println!("cargo:rerun-if-env-changed=MLN_CLONE_REPO");
     println!("cargo:rerun-if-env-changed=MLN_FROM_SOURCE");
     let cpp_root = env::var_os("MLN_FROM_SOURCE").map(PathBuf::from);
@@ -223,33 +325,11 @@ fn clone_or_download(root: &Path) -> PathBuf {
         sub_module
     } else {
         // Defaults to downloading the static library
-        #[cfg(target_os = "ios")]
-        let url = "https://github.com/louwers/maplibre-native/releases/download/core-e4739013392a18e26956471b57480dd3950d33db/libmaplibre-native-core-ios-metal.a";
-        #[cfg(target_os = "linux")]
-        let url = "http://localhost:8000/libimpl.a";
-        // let url = "https://github.com/louwers/maplibre-native/releases/download/core-e4739013392a18e26956471b57480dd3950d33db/libmaplibre-native-core-linux-opengl.a";
-        #[cfg(target_os = "macos")]
-        let url = "https://github.com/louwers/maplibre-native/releases/download/core-e4739013392a18e26956471b57480dd3950d33db/libmaplibre-native-core-macos-metal.a";
-
-        println!(
-            "cargo:warning=Downloading precompiled maplibre-native core library from {url} into {}",
-            out_dir.display()
-        );
-        fs::create_dir_all(&out_dir).expect("Failed to create output directory");
-        let mut downloader = Downloader::builder()
-            .download_folder(&out_dir)
-            .parallel_requests(1)
-            .build()
-            .expect("Failed to create downloader");
-        let mut iter = downloader
-            .download(&[Download::new(url)])
-            .expect("Failed to download maplibre-native static lib")
-            .into_iter();
-        let (Some(result), None) = (iter.next(), iter.next()) else {
-            panic!("Unexpected number of results from downloader");
-        };
+        let (library_file, headers) = download_static(&out_dir, MLN_REVISION);
+        let extracted_path = out_dir.join("headers");
+        extract_headers(&headers, &extracted_path);
         // Returning the downloaded file, bypassing CMakeLists.txt check
-        return result.expect("Error downloading static MLN").file_name;
+        return (library_file, vec![root.join("include"),extracted_path.join("include")]);
     };
 
     let check_cmake_list = cpp_root.join("CMakeLists.txt");
@@ -259,7 +339,25 @@ fn clone_or_download(root: &Path) -> PathBuf {
         check_cmake_list.display(),
     );
 
-    cpp_root
+    // TODO: This is a temporary solution. We should get this list from CMake as well.
+    let mut include_dirs = vec![
+        root.join("include"),
+        cpp_root.join("include"),
+        cpp_root.join("platform/default/include"),
+    ];
+    if cpp_root.is_dir() {
+        for entry in WalkDir::new(cpp_root.join("vendor")) {
+            let entry = entry.expect("Failed reading maplibre-native/vendor directory");
+            if entry.file_type().is_dir()
+                && !entry.path_is_symlink()
+                && entry.file_name() == "include"
+            {
+                include_dirs.push(entry.path().to_path_buf());
+            }
+        }
+    };
+
+    (cpp_root, include_dirs)
 }
 
 /// Build the "mbgl-core-deps" target first so that mbgl-core-deps.txt is generated.
@@ -296,21 +394,7 @@ fn build_static_lib(cpp_root: &Path) {
 }
 
 /// Gather include directories and build the C++ bridge using `cxx_build`.
-fn build_bridge(root: &Path, cpp_root: &Path) {
-    // TODO: This is a temporary solution. We should get this list from CMake as well.
-    let mut include_dirs = vec![
-        root.join("include"),
-        cpp_root.join("include"),
-        cpp_root.join("platform/default/include"),
-    ];
-    for entry in WalkDir::new(cpp_root.join("vendor")) {
-        let entry = entry.expect("Failed reading maplibre-native/vendor directory");
-        if entry.file_type().is_dir() && !entry.path_is_symlink() && entry.file_name() == "include"
-        {
-            include_dirs.push(entry.path().to_path_buf());
-        }
-    }
-
+fn build_bridge(include_dirs: Vec<PathBuf>) {
     println!("cargo:rerun-if-changed=src/renderer/bridge.rs");
     println!("cargo:rerun-if-changed=include/map_renderer.h");
     cxx_build::bridge("src/renderer/bridge.rs")
@@ -325,7 +409,7 @@ fn build_bridge(root: &Path, cpp_root: &Path) {
 
 fn build_mln() {
     let root = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let cpp_root = clone_or_download(&root);
+    let (cpp_root, include_dirs) = clone_or_download(&root);
     if cpp_root.is_dir() {
         add_link_targets(&cpp_root);
         build_static_lib(&cpp_root);
@@ -341,7 +425,7 @@ fn build_mln() {
                 .expect("Invalid UTF-8 in static MLN filename")
         );
     }
-    build_bridge(&root, &cpp_root);
+    build_bridge(include_dirs);
 }
 
 fn main() {
